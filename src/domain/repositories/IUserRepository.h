f07a8109993d94208ffb725252632b74bfc23dec
LinAung-Thet <laungthet@gmail.com>
2026-08-12 19:55:22 +1200

    Added IUserRepository.h for the interface of user repositories.

---
+++ src/domain/repositories/IUserRepository.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <optional>
+#include <string>
+#include "../entities/User.h"
+
+namespace domain::repositories {
+
+class IUserRepository {
+public:
+    virtual ~IUserRepository() = default;
+
+    virtual void add(const entities::User& user) = 0;
+    virtual std::optional<entities::User> findByEmail(const std::string& email) const = 0;
+};
+
+} // namespace domain::repositories
